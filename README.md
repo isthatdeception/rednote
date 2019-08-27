98cceceb0c0c9d60a28988afb56848a41b2d45a1
Aditya Mehra <aditya97mehra@gmail.com>
2019-08-27 21:33:31 +0530

    initial boiler plate of rednote

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rednote
 to do list app
+it is based on firebase and react. that makes it future proof and will be designed as it can be simply great and easy to use.
+
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>hey there!</p>
+    </header>
+  </div>
+);
+
+export default App;
